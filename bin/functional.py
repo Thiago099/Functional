763daa509455b4b5a6d9a528a787e32e623b3e4e
Thiago <thiago.kaique01@gmail.com>
2021-07-31 11:23:55 -0300

    [feature] array input

--- bin/functional.py
+++ bin/functional.py
@@ -18,7 +18,7 @@ def get_command(command):
     db.close()
     return result[0]
 
-def build(classe, name, parameter = None):
+def build(classe, name, parameter = []):
     db = sql('functional')
     
     
@@ -26,7 +26,10 @@ def build(classe, name, parameter = None):
 
     if(not base):
         return 'command not found'
-    def next(id_child):
+    
+    scope = set()
+
+    def next(id_child, scope):
         child = db.query(f'''
             SELECT
                 *
@@ -35,6 +38,8 @@ def build(classe, name, parameter = None):
             WHERE
                 parent = {id_child}
         ''')
+
+        
         
         ret = ''
         for i in child:
@@ -51,7 +56,9 @@ def build(classe, name, parameter = None):
                     id = {child}
             ''')[0]
 
-            cur = next(child) + data['value']
+            
+
+            cur = next(child, scope) + data['value']
 
             
             parameter = db.query(f'''
@@ -61,10 +68,16 @@ def build(classe, name, parameter = None):
                     sub_command_parameter
                 WHERE
                     sub_command = {id}
-            ''')
+            ''')    
 
             for i in parameter:
+
+                if i['parameter'] in scope:
+                    scope.remove(i['parameter'])
+                
                 command = i['command']
+                
+                
                 if(command):
                     base = db.query(f'''
                         SELECT
@@ -74,36 +87,64 @@ def build(classe, name, parameter = None):
                         WHERE
                             id = {command}
                         ''')[0]
-                    cur = cur.replace('{{'+i['parameter']+'}}', base['value'] + next(command))
+                    cur = cur.replace('{{'+i['parameter']+'}}', base['value'] + next(command, scope))
                 else:
                     cur = cur.replace('{{'+i['parameter']+'}}', i['value'])
 
             ret += cur + '\n'
+        command_parameter = db.query(f'''
+            SELECT
+                *
+            FROM
+                command_parameter
+            WHERE
+                command = {id_child}
+        ''')
+
+        for i in command_parameter:
+            scope.add(i['parameter'])
         return ret
     
-    ret = next(base['id']) + base['value']
-
-    if(parameter):
+    ret = next(base['id'], scope) + base['value']
+    if(type(parameter) is dict):
         for i in parameter:
             if(type(parameter[i]) is tuple):
                 command = get_command(parameter[i])
-                ret = ret.replace('{{'+i+'}}', next(command['id']) + command['value'])
+                ret = ret.replace('{{'+i+'}}', next(command['id']) + command['value'], scope)
             else:
                 ret = ret.replace('{{'+i+'}}', parameter[i])
+    else:
+        scope = list(scope)
+        for i in range(len(scope)):
+                if(i >= len(parameter)):
+                    ret = ret.replace('{{'+scope[i]+'}}', '{{'+str(i)+'}}')
+                else:
+                    if(type(parameter[i]) is tuple):
+                        command = get_command(parameter[i])
+                        ret = ret.replace('{{'+scope[i]+'}}', next(command['id']) + command['value'], scope)
+                    else:
+                        ret = ret.replace('{{'+scope[i]+'}}', parameter[i])
     db.close()
     return ret
 
-def create_command(classe, name, value = ''):
+def create_command(classe, name, value = '', parameter = None):
     db = sql('functional')
-    db.run(build('sql','insert into',
+    id = db.run(build('sql','insert into',
     {
-        'table':'command',
-        'field':'class, name, value',
-        'value': f"'{classe}', '{name}', '{value}'"
+        'table': 'command',
+        'field': 'class, name, value',
+        'value':  f"'{classe}', '{name}', '{value}'"
     }))    
+    if(parameter):
+        for i in parameter:
+            db.run(build('sql','insert into',{
+                'table':'command_parameter',
+                'field':'command, parameter',
+                'value': f"{id}, {i}"
+            }))
     db.close()
 
-def set_child(command, child, parameter = None):
+def set_child(command, child, parameter = None, new_parameter = None):
     parent = get_command(command)
     child = get_command(child)
     error = False
@@ -127,16 +168,10 @@ def set_child(command, child, parameter = None):
     }))    
     if(parameter):
         for i in parameter:
-            if(type(parameter[i]) is tuple):
-                db.run(build('sql','insert into',{
-                    'table': 'sub_command_parameter',
-                    'field': 'sub_command, parameter, value, command',
-                    'value': f"{id}, '{i}', null, {get_command(parameter[i])['id']}"
-                }))
-            else:
-                db.run(build('sql','insert into',{
-                    'table': 'sub_command_parameter',
-                    'field': 'sub_command, parameter, value, command',
-                    'value': f"{id}, '{i}', '{parameter[i]}', null"
-                }))
+            db.run(build('sql','insert into',{
+                'table': 'sub_command_parameter',
+                'field': 'sub_command, parameter, value, command',
+                'value': f"{id}, '{i}', null, {get_command(parameter[i])['id'] if type(parameter[i]) is tuple else parameter[i]}"
+            }))
+    
     db.close()
