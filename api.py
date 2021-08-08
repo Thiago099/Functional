140c23f3fc9f0359234f8e2a2036963bfddd0c6e
Thiago <thiago.kaique01@gmail.com>
2021-08-08 10:07:19 -0300

    [feature] build on the api

--- api.py
+++ api.py
@@ -1,8 +1,24 @@
+from flask import Flask as fl, request as rq
 import sys
 sys.path.insert(1, 'bin')
 sys.path.insert(1, 'functional')
 
+from persistence import sql
+import functional as fn
+from sql_flask import table
+import vector as vc
+import json
 from rest import server
 
 
-server('functional')
+api = fl(__name__)
+
+server(api, 'functional')
+
+@api.route('/build/<id>', methods = ['GET'])
+def database(id):
+    db = sql('functional')
+    cmd = db.query(f'SELECT * FROM command WHERE id = {id}')[0]
+    db.close()
+    return  fn.build(cmd['class'],cmd['name']), 200, {'Content-Type': 'text/plain; charset=utf-8'}
+api.run()
--- functional/rest.py
+++ functional/rest.py
@@ -8,11 +8,8 @@ from sql_flask import table
 import vector as vc
 import json
 
-def server(database):
-    global api
-    api = fl(__name__)
+def server(api, database):
     db = sql('information_schema')
     for i in db.query(f'SELECT TABLE_NAME FROM TABLES WHERE TABLE_SCHEMA = \'{database}\''):
         exec(fn.build('flask', 'crud', [i['TABLE_NAME'], database]))
     db.close()
-    api.run()
