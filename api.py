122ee193a3dca2cb5c16ebdccec793b74ddeda62
Thiago <thiago.kaique01@gmail.com>
2021-08-08 10:36:43 -0300

    [feature] get child commands endpoint

--- api.py
+++ api.py
@@ -16,10 +16,17 @@ api = fl(__name__)
 server(api, 'functional')
 
 @api.route('/build/<id>', methods = ['GET'])
-def database(id):
+def build(id):
     db = sql('functional')
     cmd = db.query(f'SELECT * FROM command WHERE id = {id}')[0]
     db.close()
     return  fn.build(cmd['class'], cmd['name']), 200, {'Content-Type': 'text/plain; charset=utf-8'}
+
+@api.route('/get_child/<id>', methods = ['GET'])
+def get_child(id):
+    db = sql('functional')
+    result = db.query(f'SELECT id, child FROM sub_command WHERE parent = {id}')
+    db.close()
+    return  json.dumps(result), 200, {'Content-Type': 'text/json; charset=utf-8'}
 
 api.run()
